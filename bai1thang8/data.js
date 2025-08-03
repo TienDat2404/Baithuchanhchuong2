4b734605892346e616b092650d2f84f606bc488a
TienDat2404 <ntdat24104@gmail.com>
2025-08-03 21:32:21 +0700

    1/8

---
+++ bai1thang8/data.js
@@ -0,0 +1,7 @@
+const employees = [
+  { id: 1, ten: "Mai", hoDem: "Thục Anh", diaChi: "23 Hoàng Đạo Thủy, Thanh Xuân, Hà Nội" },
+  { id: 2, ten: "Hoàng", hoDem: "Sinh Hùng", diaChi: "23 Hoàng Đạo Thủy, Thanh Xuân, Hà Nội" },
+  { id: 3, ten: "Tình", hoDem: "Mai Trung", diaChi: "1411 Đường Láng, Cầu Giấy, Hà Nội" },
+  { id: 4, ten: "Hồng", hoDem: "Nguyễn Văn", diaChi: "1411 Đường Láng, Cầu Giấy, Hà Nội" }
+];
+export default employees;
